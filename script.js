383252b256c5f455ae16767b74a4db9136203045
Jason Breedlove <jason@jasonbreedlove.com>
2022-04-19 15:48:02 -0700

    added line-through for done items

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 'use strict';
 
-let items = [];
+// let items = [];
 
-function addItem(){
-    let input = document.getElementById('input');
-    items.push(input.value);
-    items.value = "";
-    for(let i = 0; i < items.length; i++){
-        document.getElementById('list').innerHTML += items[i] + '<br>';
-        document.createElement('button');
+function addItem() {
+    let item = document.createElement('li');
+    let input = document.getElementById('new-todo');
+    item.innerHTML = input.value;
+    // items.push(input.value);
+    input.value = "";
+    document.getElementById('toDo').appendChild(item);
+    item.addEventListener('click', deleteItem);
+}
+
+function deleteItem() {
+    this.className = 'done';
+    this.removeEventListener('click', deleteItem);
+}
+
+function undoneItems() {
+    let undone = document.getElementsByClassName('undone');
+    for (let i = 0; i < undone.length; i++) {
+        undone[i].addEventListener('click', deleteItem);
     }
+}
 
-    // document.getElementById('list').innerHTML = items.join('<br>');
-}
-function removeItems(){
-}
+// document.getElementById('list').innerHTML = items.join('<br>' + '<button class = ".linethrough" onclick="removeItem(this)">X</button>');
+// for(let i = 0; i < items.length; i++){
+// document.getElementById('list').innerHTML += items[i] + '<br>' + '<button onclick="removeItem(' + i + ')">Remove</button>' + '<br>';
+// }
+
+// document.createElement('button');
+// document.getElementById('list').innerHTML = items.join('<br>');
+
 //create encapsulated task
 // attach to dom when ready
 // learn more about innerHTML not used often, problems with it
