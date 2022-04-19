7646b0858173533150aa2ae6f93b130966955026
Jason Breedlove <jason@jasonbreedlove.com>
2022-04-18 18:11:56 -0700

    initial commit

--- script.js
+++ script.js
@@ -8,9 +8,14 @@ function addItem(){
     items.value = "";
     for(let i = 0; i < items.length; i++){
         document.getElementById('list').innerHTML += items[i] + '<br>';
+        document.createElement('button');
     }
 
     // document.getElementById('list').innerHTML = items.join('<br>');
 }
 function removeItems(){
 }
+//create encapsulated task
+// attach to dom when ready
+// learn more about innerHTML not used often, problems with it
+// javascript object, id, ischecked
